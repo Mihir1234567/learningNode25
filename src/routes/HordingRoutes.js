c45ad51b140490bbde93d9a09c2aa784576f5a1a
Mihir1234567 <79329939+Mihir1234567@users.noreply.github.com>
2025-03-20 08:13:42 +0530

    changea api link

--- src/routes/HordingRoutes.js
+++ src/routes/HordingRoutes.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 const hordingController = require("../controller/HordingController");
 routes.post("/addHording", hordingController.addHordings);
-routes.get("/", hordingController.getHordings);
+routes.get("/getAllHoardings", hordingController.getHordings);
 routes.get("/:id", hordingController.getHordingsById);
 routes.delete("/deleteHording/:id", hordingController.deleteHordingsById);
 routes.get(
